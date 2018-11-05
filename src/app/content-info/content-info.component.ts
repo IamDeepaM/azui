76d515d2dbdac52f6be31510d51ac7ae0a4cbd60
yogeshds10 <yogeshds10@gmail.com>
2018-11-05 16:22:28 +0530

    search and highlight

--- src/app/content-info/content-info.component.ts
+++ src/app/content-info/content-info.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { RuleModel } from '../add-rule/rule.model';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-content-info',
@@ -9,10 +10,34 @@ import { RuleModel } from '../add-rule/rule.model';
 export class ContentInfoComponent implements OnInit {
 
   @Input() rule: RuleModel;
+  query: string;
+  content: any;
 
-  constructor() { }
+  constructor(private toastr: ToastrService) {}
 
   ngOnInit() {
+    const vm = this;
+    vm.content = vm.rule.content;
+  }
+
+  onSearch() {
+    const vm = this;
+    if (vm.query) {
+      vm.rule.content = vm.content.replace(new RegExp(vm.query, 'gi'), match => {
+        return '<span class="highlight-text">' + match + '</span>';
+      });
+    }
+  }
+
+  onSetInfo() {
+    const vm = this;
+    if (!vm.query) {
+      vm.toastr.error('Search query shouldn\'t be empty');
+    } else {
+      vm.rule.content = vm.content.replace(new RegExp(vm.query, 'gi'), match => {
+        return '<span class="highlight-text">' + match + '</span>';
+      });
+    }
   }
 
 }
