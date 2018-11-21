cfd072444bae1546bbe5b50be1284b2379ba845c
yogeshds10 <yogeshds10@gmail.com>
2018-11-21 13:03:48 +0530

    dynamic component for rule view

--- src/app/view-rule/view-rule.component.ts
+++ src/app/view-rule/view-rule.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { NgModule, NgModuleRef, Component, OnInit, Input, ViewChild,
+ViewContainerRef, Compiler, Injector, ComponentRef} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { RuleModel } from '../add-rule/rule.model';
 
 @Component({
@@ -9,11 +12,53 @@ import { RuleModel } from '../add-rule/rule.model';
 export class ViewRuleComponent implements OnInit {
 
   @Input() rule: RuleModel;
+  @ViewChild('ruleViewContainer', {read: ViewContainerRef}) rvc: ViewContainerRef;
+  private cmpRef: ComponentRef<any>;
 
-  constructor() { }
+  constructor(private compiler: Compiler,
+    private injector: Injector,
+    private moduleRef: NgModuleRef<any>) { }
 
   ngOnInit() {
     const vm = this;
+    const start = '<div class="vrc flx-row-na"><div class="col-md-6 ch ch-l">';
+    const right = '</div><div class="col-md-6 ch ch-r">';
+    const image = '<img src="{{imgUrl}}" class="over-image" *ngIf="imgUrl">';
+    const desc = '<div class="over-data" *ngIf="description"><div>Description :</div><div class="desc-view">{{description}}</div></div>';
+    const end = '</div></div>';
+    const content = start + vm.rule.content + right + image + desc + end;
+    vm.createComponentFromRaw(content);
   }
 
+  createComponentFromRaw(template: string, styles = null) {
+    const vm = this;
+
+    const tmpCmp = Component({ template, styles })(class {
+      private imgUrl: String = null;
+      private description: String = null;
+      ngOnInit() {}
+      onDataOver(event) {
+        const dvm = this;
+        dvm.imgUrl = event.target.attributes.link.value;
+        dvm.description = event.target.attributes.desc.value;
+      }
+      onClear() {
+        const dvm = this;
+        dvm.description = '';
+        dvm.imgUrl = null;
+      }
+    });
+
+    const tmpModule = NgModule({
+      imports: [RouterModule, CommonModule],
+      declarations: [tmpCmp],
+    })(class {});
+
+    vm.compiler.compileModuleAndAllComponentsAsync(tmpModule).then((factories) => {
+      const f = factories.componentFactories[factories.componentFactories.length - 1];
+      vm.cmpRef = f.create(vm.injector, [], null, vm.moduleRef);
+      vm.cmpRef.instance.name = 'dynamic-rule-view-component';
+      vm.rvc.insert(vm.cmpRef.hostView);
+    });
+  }
 }
