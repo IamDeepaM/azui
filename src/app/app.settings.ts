bb20f00af50a9ff974d6ba9a79566c14f930fb91
yogeshds10 <yogeshds10@gmail.com>
2019-06-21 19:23:21 +0530

    app settings

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -1,3 +1,4 @@
 export class AppSettings {
-    public static api_endpoint = 'http://localhost:9000';
+    public static api_endpoint = 'http://localhost:9000';  // Local
+    // public static api_endpoint = 'http://176.223.135.127:9000';  // Production
 }
