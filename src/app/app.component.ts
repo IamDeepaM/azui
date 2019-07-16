2b8ba94d148fb679ccefe02f53b5b2cb71d89c43
yogeshds10 <yogeshds10@gmail.com>
2019-07-16 22:57:24 +0530

    nav links based on role

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
@@ -10,13 +11,18 @@ export class AppComponent {
   isExpand = true;
   routeName: string;
   isLogin: boolean;
+  isAdmin: boolean;
+  user: any;
 
   constructor(private router: Router,
-    private route: ActivatedRoute) {
+    private route: ActivatedRoute,
+    private appService: AppService) {
       const vm = this;
       vm.router.events.forEach((event) => {
         if (event instanceof NavigationEnd) {
           vm.isLogin = false;
+          vm.user = vm.appService.userObj;
+          vm.isAdmin = vm.user && vm.user.isAdmin;
           let currentRoute = this.route.root;
           if (currentRoute.children[0] !== undefined) {
             currentRoute = currentRoute.children[0];
